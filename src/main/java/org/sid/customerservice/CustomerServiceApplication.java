ca3ee73cf052165c04c65750a9bd6245d44fc9f4
Ettalbi-Zakaria <z.ettalbi@etu.enset-media.ac.ma>
2023-10-28 18:04:15 +0100

    customer-service done

--- src/main/java/org/sid/customerservice/CustomerServiceApplication.java
+++ src/main/java/org/sid/customerservice/CustomerServiceApplication.java
@@ -1,7 +1,11 @@
 package org.sid.customerservice;
 
+import org.sid.customerservice.entities.Customer;
+import org.sid.customerservice.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
@@ -9,5 +13,15 @@ public class CustomerServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerServiceApplication.class, args);
 	}
-
+	@Bean
+	CommandLineRunner commandLineRunner(CustomerRepository customerRepository){
+		return args -> {
+			customerRepository.save(new Customer(null,"Zakaria", "zakaria@gmail.com"));
+			customerRepository.save(new Customer(null,"Hamza", "hamza@gmail.com"));
+			customerRepository.save(new Customer(null,"Mohamed", "mohamed@gmail.com"));
+			customerRepository.findAll().forEach(customer -> {
+				System.out.println(customer.toString());
+			});
+		};
+	}
 }
---
+++ src/main/java/org/sid/customerservice/repositories/CustomerRepository.java
@@ -0,0 +1,10 @@
+package org.sid.customerservice.repositories;
+
+import org.sid.customerservice.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+
+@RepositoryRestResource
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
